cbc13182cc6845e55ca89beaba6b72a20cdbcb22
magicianessuwu <147211721+magicianessuwu@users.noreply.github.com>
2024-04-28 11:58:54 +0200

    Undo merge pull request
    The changes do not comply with the design of cubyz.

--- scripts/markdown_to_html_turner.js
+++ scripts/markdown_to_html_turner.js
@@ -8,56 +8,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
     markdownDiv.innerHTML = "<a href=" + markdownDiv.innerHTML + ">View the original source file here</a><br><a href=\"https://github.com/PixelGuys/Cubyz-Website/tree/master\">The html below doesn't look correct? Make a ticket here!</a>";
 
-    // Fetch the Markdown file
     fetch(markdownUrl)
         .then(response => response.text())
         .then(markdown => {
-            // Split Markdown into lines
             var lines = markdown.split('\n');
             var containerDiv;
-
-            // Process each line individually
             for (var i = 0; i < lines.length; i++) {
                 if (lines[i].startsWith("# ")) {
-                    // If line starts with "# ", create a new container div
                     containerDiv = document.createElement("div");
                     containerDiv.classList.add("container");
-                    // Create a new h1 element for the title
                     var title = document.createElement("p");
                     title.classList.add("title")
-                    title.textContent = lines[i].substring(2); // Remove the "# " prefix
+                    title.textContent = lines[i].substring(2); 
                     containerDiv.appendChild(title);
-                    // Create a banana div
                     var bananaDiv = document.createElement("p");
                     containerDiv.appendChild(bananaDiv);
                     markdownDiv.appendChild(containerDiv);
                 } else {
-                    // If no container div exists, create a new container div
                     if (!containerDiv) {
                         containerDiv = document.createElement("div");
                         markdownDiv.appendChild(containerDiv);
                     }
-
                     var bananaDivs = containerDiv.querySelectorAll("p");
                     var bananaDiv = bananaDivs[bananaDivs.length - 1];
-
-                    // Check if line is empty
                     bananaDiv.innerHTML += "<br>";
                     if (lines[i].trim() === "") {
-                        // If line is empty, add <br> tag
                         bananaDiv.innerHTML += "";
                     } else {
-                        // If line is not empty, add text inside banana div
-                        if (lines[i].startsWith("## ") || lines[i].startsWith("### ") || lines[i].startsWith("### ")) {
-                            bananaDiv.innerHTML += "<span class=\"heading\">"+lines[i]+"</span>";
+                        if (lines[i].startsWith("## ") || lines[i].startsWith("### ") || lines[i].startsWith("### ") || lines[i].startsWith("#### ")) {
+                            bananaDiv.innerHTML += "<span class=\"heading\">"+lines[i].replace("#", "").replace("#", "").replace("#", "").replace("#", "")+"</span>";
                         } else {
-                            bananaDiv.innerHTML += lines[i].replace(/\[([^\]]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>'); // Add new line
+                            bananaDiv.innerHTML += lines[i].replace(/\[([^\]]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>'); 
                         }
                     }
                 }
-
-                // Check if the next line starts with "# "
-                // If it does, nullify the container div so that a new one can be created
                 if (i + 1 < lines.length && lines[i + 1].startsWith("# ")) {
                     containerDiv = null;
                 }
