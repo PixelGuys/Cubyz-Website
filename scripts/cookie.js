1635c7c67c50b17a652b3996b3eef19475cf0fdd
magicianessuwu <147211721+magicianessuwu@users.noreply.github.com>
2024-05-01 14:39:24 +0200

    cookie functionality and fixing stuff

---
+++ scripts/cookie.js
@@ -0,0 +1,17 @@
+// Currently this file is not needed
+
+function allowCookies() {
+    document.getElementById('popup').style.display = 'none';
+
+    // Put code here to save data to browser cookies
+}
+
+function disallowCookies() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+function showCookiePopup() {
+    document.getElementById('popup').style.display = 'block';
+
+    // Use this if you want to show the cookie popup
+}
