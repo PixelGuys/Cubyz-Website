665e54dc53273e866aa44c0216eb4268604fa77a
tillplusplus <tillpp@users.noreply.github.com>
2026-03-10 08:49:14 +0100

    Use Vue

---
+++ frontEnd/src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const message = ref("You did it!")
+onMounted(async()=>{
+  try {
+    const res = await fetch("http://localhost:3000/test/1")
+    const data = await res.json()
+    message.value = data.message
+  } catch (error) {
+    console.log(error)
+    message.value = String(error)
+  }
+})
+</script>
+
+<template>
+      <nav>
+      <div class="banner">
+          <img src="/public/assets/images/green_purple_crystals.png" alt="">
+          <div>
+              <img class="logo" src="/public/assets/textures/pixelguys.png" alt="">
+              <RouterLink to="/">Home</RouterLink>
+              <RouterLink to="/">Features (no page)</RouterLink>
+              <!-- Go add a page i guess? -->
+              <RouterLink to="/information">Information</RouterLink>
+          </div>
+          <p id="modt-text">
+              "Why is nothing here?" - you (probably)
+          </p> 
+      </div>
+  </nav>
+  <main>
+        <RouterView></RouterView>
+    </main>
+     
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+}
+</style>
---
+++ frontEnd/src/pages/Index.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <p style="text-align: center;"> <!--Change the "text-align: center" to "display none" if you want it gone.-->
+            Embark on an thrilling journey across vast landscapes, <br>exploring the deepest of caves <br>unseen by
+            daylight.
+            Discover ancient minerals, untouched by time, <br>as you discover more in this forgotten
+            <br>world.
+        </p>
+        <div class="flexbox">
+            <div class="container">
+                <p class="title">
+                    Free and Open Source
+                </p>
+                <p>
+                    You can also become a contributor and make miracles in Cubyz's development.<br>
+                    <button onclick="window.location.href='https://github.com/PixelGuys/Cubyz'">Github</button><button
+                        onclick="window.location.href='https://github.com/PixelGuys/Cubyz/issues'">Issues</button><button
+                        onclick="window.location.href='https://github.com/PixelGuys/Cubyz/wiki/Addons'">Make
+                        Addons</button>
+                </p>
+            </div>
+            <div class="container">
+                <p class="title">
+                    Installation in Four Clicks
+                </p>
+                <p>
+                    The guide can be found <a href="https://github.com/PixelGuys/Cubyz/blob/master/README.md">here</a>.
+                </p>
+            </div>
+        </div>
+        <div class="container">
+            <p class="title">
+                Features
+            </p>
+            <p>
+                Cubyz is a limitless indie game which uses voxels as a base, but don't think that's all! We have a
+                ton of our own features. If you want your own features, you can expand the game with addons. <br><br>
+                TL;DR: Like a similar cube game, but with more possibilities. <br><br>
+                - No height limit <br>
+                - No depth limit <br>
+                - Farther render distances <br>
+                - Immersive fog of all colors <br>
+                - Crystals of all colors <br>
+                - Colored glass that filters light <br>
+                - Mod and addon friendly
+            </p>
+        </div>
+        <div class="container">
+            <p class="title">
+                Some screenshots
+            </p>
+            <div class="images">
+                <img style="width: 100%" src="/assets/images/colorful_crystals.png" alt="">
+                <img style="width: 100%" src="/assets/images/treetops.png" alt="">
+                <img style="width: 100%" src="/assets/images/icymountain.png" alt="">
+                <img style="width: 100%" src="/assets/images/green_crystals.png" alt="">
+                <img style="width: 100%" src="/assets/images/island.png" alt="">
+            </div>
+        </div>
+</template>
